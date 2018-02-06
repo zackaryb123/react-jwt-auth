5efd7aa38ca2bdc31cbf0ac5492a81c19db2ce48
Zack Blaylock <zackblaylock@gmail.com>
2018-02-06 08:30:07 -0500

    git init

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -4,6 +4,7 @@ import {registerUser} from '../actions/users';
 import {login} from '../actions/auth';
 import Input from './input';
 import {required, nonEmpty, matches, length, isTrimmed} from '../validators';
+
 const passwordLength = length({min: 10, max: 72});
 const matchesPassword = matches('password');
 
@@ -24,9 +25,17 @@ export class RegistrationForm extends React.Component {
                     this.onSubmit(values)
                 )}>
                 <label htmlFor="firstName">First name</label>
-                <Field component={Input} type="text" name="firstName" />
+                <Field
+                    component={Input}
+                    type="text"
+                    name="firstName"
+                />
                 <label htmlFor="lastName">Last name</label>
-                <Field component={Input} type="text" name="lastName" />
+                <Field
+                    component={Input}
+                    type="text"
+                    name="lastName"
+                />
                 <label htmlFor="username">Username</label>
                 <Field
                     component={Input}
